b33a5fae06af458d43edd66da46c60e55ce07eca
DiabateJS <diabatejs@yahoo.fr>
2020-12-24 10:07:07 +0100

    Ajout nouveau menu dans la sidebar.

---
+++ src/app/learning-base/learning-base.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-learning-base',
+  templateUrl: './learning-base.component.html',
+  styleUrls: ['./learning-base.component.css']
+})
+export class LearningBaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/train-network/train-network.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-train-network',
+  templateUrl: './train-network.component.html',
+  styleUrls: ['./train-network.component.css']
+})
+export class TrainNetworkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
